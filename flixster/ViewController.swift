d928f4055674fcafe5f13a6da83935a2773c66d3
kadija45 <87451905+kadija45@users.noreply.github.com>
2021-09-08 14:53:43 -0400

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Kadija Koroma on 9/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
