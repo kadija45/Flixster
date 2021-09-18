2da470df4d26c623c76a1cd2a691f0b23c02a777
kadija45 <87451905+kadija45@users.noreply.github.com>
2021-09-18 11:50:41 -0400

    HW2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Kadija Koroma on 9/17/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
